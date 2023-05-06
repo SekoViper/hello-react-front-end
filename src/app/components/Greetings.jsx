926c10b8b2f9467f2220e42596fd2400c926ad73
SekoViper <damehlivingstone1@gmail.com>
2023-05-06 12:58:34 +0000

    Modify greetings component.

--- src/app/components/Greetings.jsx
+++ src/app/components/Greetings.jsx
@@ -1,36 +1,36 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchGreeting } from '../redux/actions/types';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/actions/types';
 
-// const Greetings = () => {
-//   const greeting = useSelector((state) => state.greeting.greeting);
-//   const isLoading = useSelector((state) => state.isLoading);
-//   const error = useSelector((state) => state.error);
-//   const dispatch = useDispatch();
+const Greetings = () => {
+  const greeting = useSelector((state) => state.greeting.greeting);
+  const isLoading = useSelector((state) => state.isLoading);
+  const error = useSelector((state) => state.error);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(fetchGreeting());
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
 
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-//   if (error) {
-//     return (
-//       <div>
-//         Error:
-//         {error}
-//       </div>
-//     );
-//   }
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
 
-//   return (
-//     <div>
-//       <h2>Random Greeting</h2>
-//       <p>{greeting}</p>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h2>Random Greeting</h2>
+      <p>{greeting}</p>
+    </div>
+  );
+};
 
-// export default Greetings;
+export default Greetings;

a5c789a78213e5749c26c2332eccedacde520c0c
SekoViper <damehlivingstone1@gmail.com>
2023-05-06 12:54:25 +0000

    Modify app.

--- src/app/redux/reducers/reducer.js
+++ src/app/redux/reducers/reducer.js
@@ -1,33 +1,33 @@
-import {
-  FETCH_GREETING_REQUEST,
-  FETCH_GREETING_SUCCESS,
-  FETCH_GREETING_FAILURE,
-} from '../actions/types';
+// import {
+//   FETCH_GREETING_REQUEST,
+//   FETCH_GREETING_SUCCESS,
+//   FETCH_GREETING_FAILURE,
+// } from '../actions/types';
 
-// Define the initial state
-const initialState = {
-  greeting: '',
-  isLoading: false,
-  error: null,
-};
+// // Define the initial state
+// const initialState = {
+//   greeting: '',
+//   isLoading: false,
+//   error: null,
+// };
 
-// Define the reducer
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_GREETING_REQUEST:
-      return { ...state, isLoading: true, error: null };
-    case FETCH_GREETING_SUCCESS:
-      return {
-        ...state,
-        greeting: action.payload,
-        isLoading: false,
-        error: null,
-      };
-    case FETCH_GREETING_FAILURE:
-      return { ...state, isLoading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
+// // Define the reducer
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case FETCH_GREETING_REQUEST:
+//       return { ...state, isLoading: true, error: null };
+//     case FETCH_GREETING_SUCCESS:
+//       return {
+//         ...state,
+//         greeting: action.payload,
+//         isLoading: false,
+//         error: null,
+//       };
+//     case FETCH_GREETING_FAILURE:
+//       return { ...state, isLoading: false, error: action.payload };
+//     default:
+//       return state;
+//   }
+// };
 
-export default reducer;
+// export default reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-import reducer from './redux/reducers/reducer';
+// import { configureStore } from '@reduxjs/toolkit';
+// import thunk from 'redux-thunk';
+// import reducer from './redux/reducers/reducer';
 
-// Create the Redux store
-const store = configureStore({
-  reducer,
-  middleware: [thunk],
-});
+// // Create the Redux store
+// const store = configureStore({
+//   reducer,
+//   middleware: [thunk],
+// });
 
-export default store;
+// export default store;

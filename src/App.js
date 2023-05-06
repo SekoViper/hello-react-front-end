46258d585ce9bba385e63a3272f6f9057b4d395e
SekoViper <damehlivingstone1@gmail.com>
2023-05-06 12:58:04 +0000

    Modify file.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './app/store';
-// import Greetings from './app/components/Greetings';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import Greetings from './app/components/Greetings';
 
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <Router>
-//         <div>
-//           <Routes>
-//             <Route exact path="/greetings" element={<Greetings />} />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </Provider>
-//   );
-// }
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/greetings" element={<Greetings />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
 
-// export default App;
+export default App;
